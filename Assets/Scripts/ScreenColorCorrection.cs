fea1adb0cf8a6483c78fe7e3f4f2f0016b7043d4
ChrisTheCoder27 <christopher.dupas@ontariotechu.net>
2024-10-27 13:29:25 -0400

    LUT can be toggled on or off

--- Assets/Scripts/ScreenColorCorrection.cs
+++ Assets/Scripts/ScreenColorCorrection.cs
@@ -5,8 +5,32 @@ using UnityEngine;
 public class ScreenColorCorrection : MonoBehaviour
 {
     public Material m_renderMaterial;
+
+    bool colorGradingActive = false;
+
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        Graphics.Blit(source, destination, m_renderMaterial);
+        if (colorGradingActive)
+        {
+            Graphics.Blit(source, destination, m_renderMaterial);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("Turn LUT off");
+            colorGradingActive = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("Turn LUT on");
+            colorGradingActive = true;
+        }
     }
 }
