ba6ebbf9ad402868d556292d431c87e01ee7575c
ChrisTheCoder27 <christopher.dupas@ontariotechu.net>
2024-10-29 10:30:15 -0400

    Added materials for other LUTs that can be toggled

--- Assets/Scripts/ScreenColorCorrection.cs
+++ Assets/Scripts/ScreenColorCorrection.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class ScreenColorCorrection : MonoBehaviour
 {
     public Material m_renderMaterial;
+    public Material m_renderMaterial2;
+    public Material m_renderMaterial3;
 
-    bool colorGradingActive = false;
+    bool colorGrading1Active = false;
+    bool colorGrading2Active = false;
+    bool colorGrading3Active = false;
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        if (colorGradingActive)
+        if (colorGrading1Active)
         {
             Graphics.Blit(source, destination, m_renderMaterial);
+        }
+        else if (colorGrading2Active)
+        {
+            Graphics.Blit(source, destination, m_renderMaterial2);
+        }
+        else if (colorGrading3Active)
+        {
+            Graphics.Blit(source, destination, m_renderMaterial3);
         }
         else
         {
@@ -24,13 +36,27 @@ public class ScreenColorCorrection : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            Debug.Log("Turn LUT off");
-            colorGradingActive = false;
+            colorGrading1Active = false;
+            colorGrading2Active = false;
+            colorGrading3Active = false;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            Debug.Log("Turn LUT on");
-            colorGradingActive = true;
+            colorGrading1Active = true;
+            colorGrading2Active = false;
+            colorGrading3Active = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            colorGrading1Active = false;
+            colorGrading2Active = true;
+            colorGrading3Active = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            colorGrading1Active = false;
+            colorGrading2Active = false;
+            colorGrading3Active = true;
         }
     }
 }
